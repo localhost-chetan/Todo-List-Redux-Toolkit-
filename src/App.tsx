73e174c472ddb2f2beb70d5d38630ed8164a6f69
Chetan Seervi <chetanseervi513@gmail.com>
2025-04-14 13:08:50 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@shadcn/button";
+
+export function App() {
+  return (
+    <>
+      <main>
+        <h1>Redux Toolkit</h1>
+
+        <Button>Add Card</Button>
+      </main>
+    </>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@redux': path.resolve(__dirname, './src/redux'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@shadcn': path.resolve(__dirname, './src/components/ui'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  }
+})
