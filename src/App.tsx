021cc07a9ca61c4fc35f7439961cb4adeddac7a4
Chetan Seervi <chetanseervi513@gmail.com>
2025-04-16 15:32:33 +0530

    Remove a paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,6 @@ export function App() {
         className={`flex min-h-screen flex-col items-center justify-center gap-y-5 py-4 select-none`}
       >
         <Todo />
-
-        <p className={`line-clamp-2 max-w-52 text-ellipsis`}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-          tempore sint explicabo ad culpa, dolorum porro facere veniam
-          obcaecati. Quae ab rem animi expedita obcaecati est modi error nobis
-          voluptas.
-        </p>
       </main>
     </>
   );
